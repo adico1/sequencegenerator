7f70f4cca7971e00fa879a24d1e5d5320edfa8fe
Adi Cohen <adico1@gmail.com>
2020-01-13 16:29:29 +0200

    release triggered

--- edition-esnext/__mocks__/get-mac.adapter.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class GetMacAddress {
-    constructor() {
-        this.getMACAddress = () => '00:00:00:00:00:00';
-        this.getMACAddressHashed = () => -1299227078;
-    }
-}
-exports.default = GetMacAddress;
